0483087f4b96a011f7c83b8587cbe1bb67d5ea6c
6704062611115 <s6704062611115@email.kmutnb.ac.th>
2024-11-28 15:19:57 +0700

    lap 1 ข้อ 3

--- lap1.c
+++ lap1.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 int main(){
-	int num1;
-	int num2;
-	printf("num1=<Enter num1> \nnum2=<Enter num2>\n");
-	scanf("%d \n %d", &num1,&num2);
+	int n1,n2;
+	int sum;
 	
-	if (num1>num2) {
-		printf("num1 greater than num2");
+	scanf("%d" , &n1);
+	scanf("%d" , &n2);
+	
+	if (n1>n2) {
+		printf("n1 greater than n2\n");
+		sum = n1 - n2;
+		printf(%d,&sum)
 	}
 	else {
-		printf("num2 greater than num1");
+		printf("n2 greater than n1\n");
+		sum = n2 - n1;
+		printf(%d,&sum)
 	}
 	
 }
