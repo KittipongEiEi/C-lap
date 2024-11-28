5dc91afe4499d6fa295b63b379e0873f3d52767b
6704062611115 <s6704062611115@email.kmutnb.ac.th>
2024-11-28 14:45:30 +0700

    Add files via upload

---
+++ lap1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int num1 = 5;
+    int num2 = 6;
+    if (num1 > num2) {
+        printf("num 1 is greater than num 2\n");
+    }
+    if (num2 > num1) {
+        printf("num 2 is greater than num 1\n");
+    }
+    return 0;
+}
