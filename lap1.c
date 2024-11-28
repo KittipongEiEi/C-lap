950619b3811b02937377b9fc6faea6dabd147fb1
6704062611115 <s6704062611115@email.kmutnb.ac.th>
2024-11-28 15:23:53 +0700

    lap 1 ข้อ 1

--- lap1.c
+++ lap1.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-int main(){
-int num1;
-int num2;
-printf("num1=<Enter num1> \nnum2=<Enter num2>\n");
-scanf("%d \n %d", &num1,&num2);
-
-if (num1>num2) {
-printf("num1 greater than num2");
-}
-else {
-printf("num2 greater than num1");
-}
-
+int main() {
+    int num1 = 5;
+    int num2 = 6;
+    if (num1 > num2) {
+        printf("num 1 is greater than num 2\n");
+    }
+    if (num2 > num1) {
+        printf("num 2 is greater than num 1\n");
+    }
+    return 0;
 }
